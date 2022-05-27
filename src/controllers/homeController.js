0ff83ee85657e23f23d1c67d90f595c46d9cace9
Gabriel Feitosa <gabrielz2x@hotmail.com>
2022-05-26 22:23:14 -0300

    ajustes front end pagina carrinho e pagamento

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,9 +1,15 @@
 const homeController = {
+    favoritos: (req, res) => {
+        res.render('home/favoritos');
+    },
     index: (req, res) => {
         res.render('home/index');
     },
-    favoritos: (req, res) => {
-        res.render('home/favoritos');
+    carrinho: (req, res) => {
+        res.render('home/meucarrinho');
+    },
+    pagamento: (req, res) => {
+        res.render('home/pagamento');
     },
     perfil: (req, res) => {
         res.render('home/perfil');
@@ -15,7 +21,7 @@ const homeController = {
         res.render('home/produtos');
     },
     quemsomos: (req, res) => {
-        res.render('home/quemsomos')
+        res.render('home/quemsomos');
     },
     segmentos: (req, res) => {
         res.render('home/segmentos');
--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/homeController');
 
-router.get('/favoritos', homeController.favoritos)/
+router.get('/favoritos', homeController.favoritos)
 router.get('/', homeController.index);
 router.get('/home', homeController.index);
+router.get('/carrinho', homeController.carrinho);
+router.get('/pagamento', homeController.pagamento);
 router.get('/perfil', homeController.perfil);
 router.get('/politicas', homeController.politicas);
 router.get('/produtos', homeController.produtos);
