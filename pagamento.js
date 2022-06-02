71e54f78ca1336bf7377bc08d83e5af66232cee2
patrikfdorneles <patrikfdorneles@gmail.com>
2022-06-02 08:12:13 -0300

    correção java pagamento

--- pagamento.js
+++ pagamento.js
@@ -1,39 +1,39 @@
-function menu1() {
+function menuPagamento() {
     const tipoPagamento = document.querySelectorAll("input[name='tipo']");  
     const cartaoClasses = document.querySelector(".pagamento-conteudo");
     const botaoMudar = document.querySelector(".botoesfinalizar");
-    const textoMostrar1 = document.querySelector(".txtp1");
-    const textoMostrar2 = document.querySelector(".txtp2");
-    const textoMostrar3 = document.querySelector(".txtp3");
+    const textoMostrar = document.querySelectorAll(".textopagamento");
     const textoModificar= document.querySelector(".descontopix p");
 
     const findSelected = () => {
         const selected = document.querySelector("input[name='tipo']:checked").value;
         cartaoClasses.classList.remove("active") 
-        textoMostrar1.classList.remove("active3")
-        textoMostrar2.classList.remove("active3")
-        textoMostrar3.classList.remove("active3")
+        textoMostrar.forEach((item) => {
+            item.classList.remove("active3")
+        });
             if (selected === "cartao") {
                 cartaoClasses.classList.add("active");
                 botaoMudar.children[1].innerHTML="Pagar com Cartão de Crédito"
-                textoMostrar3.classList.add("active3");
+                textoMostrar[2].classList.add("active3");
                 textoModificar.innerHTML="Pagamento via Cartão de Crédito"
             } else if (selected === "pix") {
                 botaoMudar.children[1].innerHTML="Pagar com PIX"
-                textoMostrar1.classList.add("active3");
+                textoMostrar[0].classList.add("active3");
                 textoModificar.innerHTML="Pagamento via PIX"
             } else if (selected === "boleto") {
+                botaoMudar.style.display = 'none';
                 botaoMudar.children[1].innerHTML="Pagar com Boleto"
-                textoMostrar2.classList.add("active3");
+                botaoMudar.style.display = 'flex';
+                textoMostrar[1].classList.add("active3");
                 textoModificar.innerHTML="Pagamento via Boleto"
             }
     }
     tipoPagamento.forEach(radio => {radio.addEventListener("change", findSelected);
     })  
 } 
-menu1();
+menuPagamento();
 
-function menu2(){
+function subMenuCartao(){
     const tipoCartao = document.querySelectorAll("input[name='tipocartao']");
     const cartaoClasse1 = document.querySelector(".novocartao-conteudo");
     const cartaoClasse2 = document.querySelector(".cartaoexistente-conteudo");
@@ -53,4 +53,4 @@ function menu2(){
     tipoCartao.forEach(radio => {radio.addEventListener("change", findSelected);
     })  
 }
-menu2();
+subMenuCartao();
