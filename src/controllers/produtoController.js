6b7072f0431165f0fc632d9354af8c9d7db03237
matheus-fnprado <matheusfilipe573@gmail.com>
2022-08-31 19:54:20 -0300

    Atualização controller

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -18,25 +18,17 @@ const produtoController = {
 
   //Realiza o cadastro de um novo produto no banco de dados
   store: async (req, res) => {
-<<<<<<< HEAD
-    const { nome, descricao, preco, ativo, categoria_id } = req.body;
-    const produtos = {
-=======
     const { nome, descricao, preco, ativo } = req.body;
-    const produto = {
->>>>>>> 8d64927b157d9853fe909f621072bcb55c6afd80
+    const produtos = {
       nome,
       descricao,
       imagem: req.file.filename,
       preco,
       categoria_id,
       ativo: ativo == "on" ? true : false,
-    };
-<<<<<<< HEAD
+    }
+
     await Produto.create(produtos);
-=======
-   await Produto.create(produto);
->>>>>>> 8d64927b157d9853fe909f621072bcb55c6afd80
     return res.redirect("/adm/produtos");
   },
 
